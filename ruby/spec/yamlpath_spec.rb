89949e93768073d42d96f4cf5f182c4189405644
Wanderson Camargo <wandersonwhcr@gmail.com>
2025-04-07 20:53:40 -0300

    Handle scalar files

--- ruby/spec/yamlpath_spec.rb
+++ ruby/spec/yamlpath_spec.rb
@@ -2,9 +2,15 @@ require 'yamlpath'
 
 describe YAMLPath do
   describe "#path" do
-    let(:empty) { File.read("./spec/fixtures/empty.yaml") }
-    it "returns root" do
+    let(:empty)  { File.read("./spec/fixtures/empty.yaml")  }
+    let(:scalar) { File.read("./spec/fixtures/scalar.yaml") }
+
+    it "returns root for empty file" do
       expect(YAMLPath.path(empty)).to eql(".")
+    end
+
+    it "returns root for scalar" do
+      expect(YAMLPath.path(scalar)).to eql(".")
     end
   end
 end
