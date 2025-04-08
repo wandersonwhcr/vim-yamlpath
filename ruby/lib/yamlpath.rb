9af67076b66f60bbbad959a0ef915a25463910bc
Wanderson Camargo <wandersonwhcr@gmail.com>
2025-04-07 21:02:14 -0300

    Initialize mapping

--- ruby/lib/yamlpath.rb
+++ ruby/lib/yamlpath.rb
@@ -1,9 +1,22 @@
+require 'yaml'
+
 class YAMLPath
   def self.hello()
     print "Hello, World"
   end
 
   def self.path(content)
-    "."
+    if not document = YAML.parse(content)
+      return "."
+    end
+
+    child = document.children.first
+
+    case child
+    when YAML::Nodes::Scalar
+      return "."
+    end
+
+    ".a"
   end
 end
--- ruby/spec/yamlpath_spec.rb
+++ ruby/spec/yamlpath_spec.rb
@@ -2,15 +2,20 @@ require 'yamlpath'
 
 describe YAMLPath do
   describe "#path" do
-    let(:empty)  { File.read("./spec/fixtures/empty.yaml")  }
-    let(:scalar) { File.read("./spec/fixtures/scalar.yaml") }
+    let(:empty)          { File.read("./spec/fixtures/empty.yaml")          }
+    let(:scalar)         { File.read("./spec/fixtures/scalar.yaml")         }
+    let(:mapping_simple) { File.read("./spec/fixtures/mapping_simple.yaml") }
 
-    it "returns root for empty file" do
+    it "returns root for empty file", :empty => true do
       expect(YAMLPath.path(empty)).to eql(".")
     end
 
-    it "returns root for scalar" do
+    it "returns root for scalar", :scalar => true do
       expect(YAMLPath.path(scalar)).to eql(".")
+    end
+
+    it "returns key for simple mapping", :mapping => true, :current => true do
+      expect(YAMLPath.path(mapping_simple)).to eql(".a")
     end
   end
 end
